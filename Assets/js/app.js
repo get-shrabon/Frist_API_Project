71c41b26742db211c681913d9c0aa8865cc8cc40
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-11-08 14:47:13 +0600

    change some code

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,4 +1,4 @@
-function loadData(searchText) {
+function loadData(searchText = "13") {
   fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     .then((res) => res.json())
     .then((data) => showData(data.data));
@@ -21,7 +21,7 @@ function showData(cards) {
     <div class="card h-100">
     <img src="${card.image}" class="card-img-top">
     <h5 class="card-title">${card.phone_name}</h5>
-    <button type="button" class="btn btn-primary">Buy Now</button>
+    <button  data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="showButton('${card.slug}')" type="button" class="btn btn-primary">Show Details</button>
     </div>
     `;
     searchContainer.appendChild(newCard);
@@ -42,4 +42,43 @@ const spinner = (isLoading) => {
     spinnerLoader.classList.add("d-none");
   }
 };
+// Show All Buttton
+const showButton = async (slug) => {
+  console.log("Show Button Clicked", slug);
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${slug}`
+  );
+  const contents = await res.json();
+  const phoneDetails = contents.data;
+   const modalBackdrop = document.getElementById("staticBackdrop");
+   const modalContainer = document.createElement("div");
+   modalContainer.classList.add("modal-dialog");
+   modalContainer.classList.add("modal-dialog-centered");
+   const modalContent = document.createElement("div");
+   modalContent.classList.add("modal-content");
+   const modalHeader = document.createElement("div");
+   modalHeader.classList.add("modal-header");
+   modalHeader.innerHTML = `
+
+`;
+   const modalBody = document.createElement("div");
+   modalBody.classList.add("modal-body");
+   modalBody.innerHTML = `
+   <img class="mx-auto" src="${phoneDetails.image}">
+  <h3 id="phone__title" class="fw-bold">${phoneDetails.name}</h3>
+  `;
+   const modalFooter = document.createElement("div");
+   modalFooter.classList.add("modal-footer");
+   modalFooter.innerHTML = `
+    <button type="button" id="close__btn" class="btn btn-secondary" data-bs-dismiss="modal" aria-label="Close">Close</button>
+  `;
+   modalContent.appendChild(modalHeader);
+   modalContent.appendChild(modalBody);
+   modalContent.appendChild(modalFooter);
+   modalContainer.appendChild(modalContent);
+   modalBackdrop.appendChild(modalContainer);
+
+  console.log(phoneDetails);
+};
+
 loadData();
